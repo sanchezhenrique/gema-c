571e7ea59886eda05b2599525c112ac335a693e1
Henrique Sanchez <henriquealcici@gmail.com>
2026-03-04 21:13:06 -0300

    Problema 1002 beecrowd

---
+++ 1002bee.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  double raio;
+  double area;
+  cin >> raio;
+  area = raio * raio * 3.14159 ;
+  
+  cout << "A=" << fixed << setprecision(4) << area << endl;
+}
